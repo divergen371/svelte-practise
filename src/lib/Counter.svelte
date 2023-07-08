394b380f48a40838ce9bd8543bf0ae84e487c251
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-09 00:26:16 +0900

    reactiveな変数とクリックイベント

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,7 +1,11 @@
 <script>
-  export let name
+  export let name = "Counter"
   let count = 0
+  const increment = () => {
+    count += 1
+  }
 </script>
 
 <h2>{name}コンポーネント</h2>
 <p>カウント: {count}</p>
+<div><button on:click={increment}>Up</button></div>
