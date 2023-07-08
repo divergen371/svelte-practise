d9fcc9531a2e85e574938c352d2064c185b8d376
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-09 01:04:09 +0900

    $をつけるとsubscriberとしてカウントされる

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,13 +1,9 @@
 <script>
   import { writable } from "svelte/store"
   export let name = "Counter"
-  let countValue
   const count = writable(0, set => {
     set(100)
   })
-  // count.subscribe(value => {
-  //   countValue = value
-  // })
   const increment = () => {
     count.update(value => {
       console.log(value)
@@ -17,9 +13,9 @@
 </script>
 
 <h2>{name}コンポーネント</h2>
-<!-- <div>
-  <p>カウント: {countValue}</p>
-</div> -->
+<div>
+  <p>カウント: {$count}</p>
+</div>
 <div>
   <button on:click={increment}>Up</button>
 </div>
