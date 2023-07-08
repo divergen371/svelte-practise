9631c95055fea9d196d4984068704cbd8de7c79e
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-09 00:53:49 +0900

    writableのsubscribe

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,7 +1,11 @@
 <script>
   import { writable } from "svelte/store"
   export let name = "Counter"
+  let countValue
   const count = writable(0)
+  count.subscribe(value => {
+    countValue = value
+  })
   const increment = () => {
     count.update(value => value + 1)
   }
@@ -9,7 +13,7 @@
 
 <h2>{name}コンポーネント</h2>
 <div>
-  <p>カウント: {$count}</p>
+  <p>カウント: {countValue}</p>
 </div>
 <div>
   <button on:click={increment}>Up</button>
