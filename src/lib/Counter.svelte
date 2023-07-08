66359c8136a63f0dcd8a7a8c5de77bddb213dde3
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-09 00:28:43 +0900

    on:clickにインラインで処理を記述することも可能である。

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,11 +1,10 @@
 <script>
   export let name = "Counter"
   let count = 0
-  const increment = () => {
-    count += 1
-  }
 </script>
 
 <h2>{name}コンポーネント</h2>
 <p>カウント: {count}</p>
-<div><button on:click={increment}>Up</button></div>
+<div>
+  <button on:click={() => (count += 1)}>Up</button>
+</div>
