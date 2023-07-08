cde1bb9259e601c37f37ce1a4904b1f4bc7b4a2e
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-09 00:50:22 +0900

    storeモジュールのwritable update

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,16 +1,15 @@
 <script>
+  import { writable } from "svelte/store"
   export let name = "Counter"
-  let count = 1
+  const count = writable(0)
   const increment = () => {
-    count += 1
+    count.update(value => value + 1)
   }
-  $: double = count * 2
 </script>
 
 <h2>{name}コンポーネント</h2>
 <div>
-  <p>カウント: {count}</p>
-  <p>ダブルカウント: {double}</p>
+  <p>カウント: {$count}</p>
 </div>
 <div>
   <button on:click={increment}>Up</button>
