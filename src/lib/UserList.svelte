cf2e9eeda752de103a3ac91d87eba350ca5fba3e
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-09 11:32:26 +0900

    awaitブロック

--- src/lib/UserList.svelte
+++ src/lib/UserList.svelte
@@ -1,17 +1,27 @@
 <script>
   import { onMount } from "svelte"
-  let users = []
-  onMount(async () => {
+  const fetchUsers = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const data = await response.json()
-    console.log(data)
-    users = data
-  })
+    const users = await response.json()
+    if (response.ok) {
+      return users
+    } else {
+      throw new Error(response.status)
+    }
+  }
+  let promise = fetchUsers()
 </script>
 
 <h2>ユーザー一覧</h2>
-<ul>
-  {#each users as user}
-    <li>{user.name}</li>
-  {/each}
-</ul>
+{#await promise}
+  <p>Loading...</p>
+{:then users}
+  <ul>
+    {#each users as user}
+      <li>{user.name}</li>
+    {/each}
+  </ul>
+{:catch error}
+  <!-- promise was rejected -->
+  <p>Something went wrong: {error.message}</p>
+{/await}
