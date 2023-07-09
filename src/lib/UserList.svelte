8d0fd5b1c4924a97fa73a3c6b4ff78a178b4373d
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-09 11:05:11 +0900

    eachブロックで配列を展開

--- src/lib/UserList.svelte
+++ src/lib/UserList.svelte
@@ -1,10 +1,17 @@
 <script>
   import { onMount } from "svelte"
+  let users = []
   onMount(async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
     const data = await response.json()
     console.log(data)
+    users = data
   })
 </script>
 
 <h2>ユーザー一覧</h2>
+<ul>
+  {#each users as user}
+    <li>{user.name}</li>
+  {/each}
+</ul>
